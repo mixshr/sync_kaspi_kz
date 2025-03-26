27aa82fc1f3fc3166c37cb6ae426dda317723591
Михаил Шаренко <mihailsarenko@MacBook-Pro-Mihail.local>
2025-03-26 15:39:04 +0500

    Init commit

---
+++ src/main/java/kz/sync/kaspi/SyncKaspiKzApplication.java
@@ -0,0 +1,13 @@
+package kz.sync.kaspi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncKaspiKzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncKaspiKzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/sync/kaspi/SyncKaspiKzApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sync.kaspi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncKaspiKzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
